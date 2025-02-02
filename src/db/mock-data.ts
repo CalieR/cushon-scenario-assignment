d40a065db7390b78e767d21ae1517b54165a0004
Calie Rushton <calielrushton@gmail.com>
2025-02-02 17:38:26 +0000

    Add correct fund name

--- src/db/mock-data.ts
+++ src/db/mock-data.ts
@@ -3,7 +3,7 @@ import { Fund } from "../types/types";
 export const funds: Fund[] = [
   {
     id: 1,
-    name: "7IM AAP Adventurous Fund C Acc",
+    name: "Cushon Equities Fund",
     description:
       "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? ",
   },
